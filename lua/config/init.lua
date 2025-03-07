10c7349a95b7e77e9789cc877f5d1953867a0e4c
Mads Byriel <madshbyriel@gmail.com>
2025-03-07 17:54:07 +0100

    Initial commit

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,24 +2,48 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- -- Fugitive
--- -- Open up the Git window from fugitive
--- vim.keymap.set("n", "<leader>g", ":G<CR>", { noremap = true })
--- 
--- -- Start a commit message
--- vim.keymap.set("n", "<leader>gc", ":G commit<CR>", { noremap = true })
--- 
--- -- Push commits
--- vim.keymap.set("n", "<leader>gp", ":G push<CR>", { noremap = true })
--- 
--- vim.keymap.set("n", "<leader>t", function ()
---     require('neo-tree.command').execute({
---         toggle = true
---     })
--- end)
+vim.opt.termguicolors = true
+vim.o.background = 'dark'
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+
+-- Show a marker indicating errors at the column
+vim.opt.signcolumn = 'yes'
+-- vim.g.netrw_liststyle = 3
+-- vim.g.netrw_banner = 0
+
+if vim.g.neovide then
+    vim.g.neovide_fullscreen = true
+    vim.o.guifont = "0xProto Nerd Font Mono:h14"
+
+end
+
+-- setup of all plugins
+require("config.lazysetup")
 
 require("config.keybinds")
-require("config.lazysetup")
 
 -- plugins
 require("plugs.telescope")
+require("plugs.lualine")
+require("plugs.noice")
+require("plugs.neo-tree")
+require("plugs.mason")
+
+vim.cmd('colorscheme tokyonight-night')
--- lua/config/lazysetup.lua
+++ lua/config/lazysetup.lua
@@ -13,18 +13,86 @@ require('lazy').setup({
 
     -- Telescope fuzzy finding
     { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
-    { "tpope/vim-fugitive",
-	    config = function()
-		    -- Optional: Add custom keybindings or settings for Fugitive
-		    vim.keymap.set('n', '<leader>g', '<cmd>Git<cr>', { desc = 'Git status' })
-		    vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = 'Git commit' })
-		    vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { desc = 'Git push' })
-		    vim.keymap.set('n', '<leader>gl', '<cmd>Git log<cr>', { desc = 'Git log' })
-		    vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<cr>', { desc = 'Git diff' })
-	    end,
-    },
-})
 
+    -- Fugitive git integration
+    { "tpope/vim-fugitive",
+        config = function()
+            -- Optional: Add custom keybindings or settings for Fugitive
+            vim.keymap.set('n', '<leader>g', '<cmd>Git<cr>', { desc = 'Git status' })
+            vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = 'Git commit' })
+            vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { desc = 'Git push' })
+            vim.keymap.set('n', '<leader>gl', '<cmd>Git log<cr>', { desc = 'Git log' })
+            vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<cr>', { desc = 'Git diff' })
+        end,
+    },
+
+    -- Treesitter syntax highlighting
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+
+    { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons', opt = true } },
+
+    { 'folke/tokyonight.nvim' },
+
+    { 'folke/noice.nvim', dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" }},
+
+    { "nvim-neo-tree/neo-tree.nvim", branch = "v3.x", dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim", } },
+
+    { "williamboman/mason.nvim" },
+
+    -- Configure blink.cmp
+    {
+        'saghen/blink.cmp',
+        -- optional: provides snippets for the snippet source
+        dependencies = 'rafamadriz/friendly-snippets',
+
+        -- use a release tag to download pre-built binaries
+        version = '*',
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
+            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
+            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+            --
+            -- All presets have the following mappings:
+            -- C-space: Open menu or open docs if already open
+            -- C-e: Hide menu
+            -- C-k: Toggle signature help
+            --
+            -- See the full "keymap" documentation for information on defining your own keymap.
+            keymap = { preset = 'default' },
+
+            appearance = {
+                -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+                -- Useful for when your theme doesn't support blink.cmp
+                -- Will be removed in a future release
+                use_nvim_cmp_as_default = true,
+                -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = 'mono'
+            },
+
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+
+            -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+            --
+            -- See the fuzzy documentation for more information
+            fuzzy = { implementation = "prefer_rust_with_warning" }
+        },
+        opts_extend = { "sources.default" }
+    }
+})
 
 -- local builtin = require('telescope.builtin')
 -- vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
@@ -33,15 +101,6 @@ require('lazy').setup({
 -- vim.keymap.set('n', '<leader>pn', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- return require('packer').startup(function(use)
---     -- FUGITIVE! Git stuff.
---     use 'tpope/vim-fugitive'
--- 
---     -- Nice coloring!
---     use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
--- 
---     -- Startup screen
---     use "startup-nvim/startup.nvim"
--- 
 --     -- LSP
 --     use {
 --         'VonHeikemen/lsp-zero.nvim',
@@ -56,15 +115,6 @@ require('lazy').setup({
 --         }
 --     }
 -- 
---     -- The line at the bottom of the screen
---     use {'ojroques/nvim-hardline'}
--- 
---     -- Glow for markdown files
---     use {"ellisonleao/glow.nvim", config = function() require("glow").setup({
---         style = "dark",
---         width = 120,
---     }) end}
--- 
 --     -- nvim-cmp
 --     use { 'neovim/nvim-lspconfig' }
 --     use { 'hrsh7th/cmp-nvim-lsp' }
@@ -72,34 +122,4 @@ require('lazy').setup({
 --     use { 'hrsh7th/cmp-path' }
 --     use { 'hrsh7th/cmp-cmdline' }
 --     use { 'hrsh7th/nvim-cmp' }
--- 
---     -- tokyonight colorscheme
---     use { 'folke/tokyonight.nvim' }
--- 
---     -- Nice command line 
---     use {
---         'folke/noice.nvim',
---         requires = {
---             "MunifTanjim/nui.nvim",
---             "rcarriga/nvim-notify",
---         }
---     }
--- 
---     -- lualine
---     use {
---         'nvim-lualine/lualine.nvim',
---         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
---     }
--- 
---     -- neo-tree, better file browsing
---     use({
---         "nvim-neo-tree/neo-tree.nvim",
---         branch = "v3.x",
---         requires = {
---             "nvim-lua/plenary.nvim",
---             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
---             "MunifTanjim/nui.nvim",
---             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
---         }
---     })
 -- end)
