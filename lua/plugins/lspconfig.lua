0d8c286609b99048fe335775c9c8e99e550113e5
Mads Byriel <madshbyriel@gmail.com>
2025-03-10 19:39:56 +0100

    Finished the things, now lsps should work...

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -16,21 +16,31 @@ return {
                 lua_ls = { }
             }
         },
-        config = function(_, opts)
-            local lspconfig = require('lspconfig')
-            for server, config in pairs(opts.servers) do
-                -- passing config.capabilities to blink.cmp merges with the capabilities in your
-                -- `opts[server].capabilities, if you've defined it
-                config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-                lspconfig[server].setup(config)
-            end
-        end,
 
         -- example calling setup directly for each LSP
         config = function()
             local capabilities = require('blink.cmp').get_lsp_capabilities()
             local lspconfig = require('lspconfig')
+
             lspconfig['lua_ls'].setup({ capabilities = capabilities })
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = { 'saghen/blink.cmp', 'williamboman/mason-lspconfig.nvim' },
+        config = function()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+            local lspconfig = require('lspconfig')
+
+            -- Setup LSP servers installed via mason-lspconfig
+            require('mason-lspconfig').setup_handlers({
+                -- Default handler for installed servers
+                function (server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+            })
         end
     }
 }
