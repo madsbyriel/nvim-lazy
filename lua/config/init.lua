ffc70f6bb2bbb8222e90b00e96cd34e10c7f9584
Mads Byriel <madshbyriel@gmail.com>
2025-03-11 18:30:56 +0100

    Added a LOT of keybinds for interacting with LSP

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -11,9 +11,19 @@ require("plugs.treesitter")
 vim.cmd('colorscheme tokyonight-night')
 
 -- Set some lsp functionality. Telescope is goated
-vim.api.nvim_set_keymap("n", "gr", '<cmd>lua require("telescope.builtin").lsp_references()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'ge', '<cmd>lua require("telescope.builtin").diagnostics()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gs', '<cmd>lua require("telescope.builtin").git_status()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>lk", '<cmd>lua require("telescope.builtin").lsp_references()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>le', '<cmd>lua require("telescope.builtin").diagnostics()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ls', '<cmd>lua require("telescope.builtin").git_status()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lr', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>es', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ek', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ej', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
 
 -- For fun, see all colorschemes
 vim.api.nvim_set_keymap('n', '<leader>cc', '<cmd>lua require("telescope.builtin").colorscheme()<CR>', { noremap = true, silent = true })
+
+-- Create a new tab from current buffer (think buffer copy)
+vim.api.nvim_set_keymap('n', '<leader>bc', '<cmd>tabnew %<CR>', { noremap = true, silent = true })
