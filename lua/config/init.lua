41f92cb44dbd22a4bba6600d19966d29ae2827c7
Mads Byriel <madshbyriel@gmail.com>
2025-03-11 14:43:19 +0100

    Added some more LSP keymaps
    Now be able to view all errors, and references of a thingy with ge and
    gr!

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -9,3 +9,10 @@ require("plugs.mason")
 require("plugs.treesitter")
 
 vim.cmd('colorscheme tokyonight-night')
+
+-- Set some lsp functionality. Telescope is goated
+vim.api.nvim_set_keymap("n", "gr", '<cmd>lua require("telescope.builtin").lsp_references()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'ge', '<cmd>lua require("telescope.builtin").diagnostics()<CR>', { noremap = true, silent = true })
+
+-- For fun, see all colorschemes
+vim.api.nvim_set_keymap('n', '<leader>cc', '<cmd>lua require("telescope.builtin").colorscheme()<CR>', { noremap = true, silent = true })
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,3 +1,4 @@
 return {
     { 'folke/tokyonight.nvim' },
+    { 'ellisonleao/gruvbox.nvim' },
 }
