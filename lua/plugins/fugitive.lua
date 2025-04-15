c4a260d34bc4eec08f1dd5d6be365072396377f6
Mads Byriel <madshbyriel@gmail.com>
2025-04-15 13:08:22 +0000

    added optional keybind gs for git status addition to g

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,7 @@ return {
         config = function()
             -- Optional: Add custom keybindings or settings for Fugitive
             vim.keymap.set('n', '<leader>g', '<cmd>Git<cr>', { desc = 'Git status' })
+            vim.keymap.set('n', '<leader>gs', '<cmd>Git<cr>', { desc = 'Git status' })
             vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = 'Git commit' })
             vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { desc = 'Git push' })
             vim.keymap.set('n', '<leader>gl', '<cmd>Git log<cr>', { desc = 'Git log' })
